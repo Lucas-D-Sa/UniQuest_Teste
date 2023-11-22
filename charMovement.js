0c7cc5c6fb30056b0a2e0c056c8a5378d0b0f715
Lucas-D-Sa <119504411+Lucas-D-Sa@users.noreply.github.com>
2023-11-22 10:28:47 -0300

    Add files via upload

---
+++ charMovement.js
@@ -0,0 +1,79 @@
+const image = document.getElementById('char');
+let positionX = 0;
+let positionY = 0;
+
+let isFlipped = false;
+
+const zoomLevel = window.devicePixelRatio;
+
+const speed = zoomLevel * 40;
+
+console.log("Nível de Zoom: " + zoomLevel);
+
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let touchStartX = 0;
+let touchStartY = 0;
+
+function handleTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  event.preventDefault();
+
+  const touchEndX = event.touches[0].clientX;
+  const touchEndY = event.touches[0].clientY;
+
+  const deltaX = touchEndX - touchStartX;
+  const deltaY = touchEndY - touchStartY;
+
+  
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    
+    if (deltaX > 0) {
+      isFlipped = false;
+      image.style.transform = 'scaleX(1)';
+      positionX += speed;
+    } else {
+      isFlipped = true;
+      image.style.transform = 'scaleX(-1)';
+      positionX -= speed;
+    }
+  } else {
+   
+    if (deltaY > 0) {
+      positionY += speed;
+    } else {
+      positionY -= speed;
+    }
+  }
+
+  image.style.left = positionX + 'px';
+  image.style.top = positionY + 'px';
+
+  touchStartX = touchEndX;
+  touchStartY = touchEndY;
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    isFlipped = true;
+    image.style.transform = 'scaleX(-1)';
+    positionX -= speed;
+  } else if (event.key === 'ArrowRight') {
+    isFlipped = false;
+    image.style.transform = 'scaleX(1)';
+    positionX += speed;
+  } else if (event.key === 'ArrowUp') {
+    positionY -= speed;
+  } else if (event.key === 'ArrowDown') {
+    positionY += speed;
+  }
+
+  image.style.left = positionX + 'px';
+  image.style.top = positionY + 'px';
+});
